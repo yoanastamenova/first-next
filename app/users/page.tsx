55564ef12e295c4f5e9ed77eb93c816ed480bd31
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-08-28 16:05:37 +0200

    fix: spaces in users page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 interface User {
     id: number;
     name: string;
@@ -8,8 +7,7 @@ interface User {
 const UsersPage = async () => {
    const res = await fetch('https://jsonplaceholder.typicode.com/users');
    const users: User[] = await res.json();
-
-
+   
   return (
     <div>
         <h1>Users</h1>
