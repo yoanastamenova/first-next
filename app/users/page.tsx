d4a37b9f3ad935a9f398f19307372e463d711728
yoanastamenova <yoana.stamenovaa@gmail.com>
2025-08-28 17:04:07 +0200

    feat: add revalidating

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,12 +5,14 @@ interface User {
 }
 
 const UsersPage = async () => {
-   const res = await fetch('https://jsonplaceholder.typicode.com/users');
+   const res = await fetch('https://jsonplaceholder.typicode.com/users', 
+    { next: { revalidate: 10 } });
    const users: User[] = await res.json();
-   
+
   return (
     <div>
         <h1>Users</h1>
+        <p>{new Date().toLocaleDateString()}</p>
         <ul>
             {users.map(user => <li key={user.id}> {user.name}</li>)}
         </ul>
